4d5cc16574fd6df75c78444521034dd513059e0c
Micah S <145581856+Zeonthestupid@users.noreply.github.com>
2026-03-01 14:28:03 -0700

    wo

--- roomgen.gd
+++ roomgen.gd
@@ -1,31 +1,32 @@
 extends Node2D
 
-
-const Spawn_Rooms: Array= [preload("res://roomas/room4.tscn")]
+const SPAWN_ROOMS: Array[PackedScene] = [preload("res://roomas/room4.tscn")]
 const Middle_Rooms: Array= [preload("res://roomas/room1.tscn"), 
 preload("res://roomas/room2.tscn"), preload("res://roomas/room3.tscn")]
 const End_Rooms: Array= [preload("res://roomas/room5.tscn")]
-
 const Tile_Size:int =  32
 const Wall_Tile:int = 2
 
-@export var num_levels:int = 3
-
+@export var num_levels:int = 5
+var roomb
 @onready var player: CharacterBody2D = get_parent().get_node("player")
 
 func _ready():
 	_spawn_rooms()
-	
+
+
+
 func _spawn_rooms():
 	var previous_room: Node2D
 	
 	for i in num_levels:
-		var room: Node2D
+		var roomspawn: Node2D
 		
 		if i == 0:
-			room = Spawn_Rooms[randi() % Spawn_Rooms.size()].instance()
-			player.position = room.get_node("playerspawn")
-		
-		add_child(room)
-		previous_room=room
+			roomb = SPAWN_ROOMS[(randi() % SPAWN_ROOMS.size())-1]
+			roomspawn = roomb.instantiate()
+			roomspawn.global_position = player.global_position
+			
+		add_child(roomspawn)
+		previous_room=roomspawn
 		
